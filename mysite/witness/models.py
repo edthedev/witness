583e0f74e1992bdaed7586b40dc4ff9a57ee6980
Edward Delaporte <edthedev@gmail.com>
2013-06-10 21:39:25 -0500

    Starting a Django 1.5 app.

---
+++ mysite/witness/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Submission(models.Model):
+    '''A user submitted data bit.'''
+    submitted_date = models.DateTimeField('Date Submitted')
+    content = models.CharField(max_length=1000)
+
+
+
