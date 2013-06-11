2c94d3a6ee43119cf7202935440df4f78e9434dd
Edward Delaporte <edthedev@gmail.com>
2013-06-10 22:34:49 -0500

    Added templates directory.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,5 +1,7 @@
 # Django settings for mysite project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -107,9 +109,7 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.expanduser('~/projects/witness/templates'),
 )
 
 INSTALLED_APPS = (
